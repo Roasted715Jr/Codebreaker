85b8de335f86058fe9c860a0ee189f42e3281cea
ShadowDragonP <ryota.sakurai@hotmail.com>
2017-08-22 18:34:40 -0400

    Add Unity project files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpHeight;
+    private float moveVelocity;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D rb2d;
+    private bool grounded;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveVelocity = 0f;
+
+		if (Input.GetAxisRaw("Vertical") == 1 && grounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpHeight);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            //rb2d.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), rb2d.velocity.y);
+        	moveVelocity = moveSpeed * Input.GetAxisRaw("Horizontal");
+        }
+
+        rb2d.velocity = new Vector2(moveVelocity, rb2d.velocity.y);
+
+        if (rb2d.velocity.x > 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (rb2d.velocity.x < 0)
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+}
